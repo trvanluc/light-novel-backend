4a8dae3bad219d5c3cb3cdbc6540b33bf8e45219
Trần Văn Lực <trvanluc.w@gmail.com>
2026-06-22 17:15:52 +0700

    db

--- src/config/database.js
+++ src/config/database.js
@@ -1,20 +1,7 @@
 require('dotenv').config();
 
 const { PrismaClient } = require('@prisma/client');
-const { Pool } = require('pg');
-const { PrismaPg } = require('@prisma/adapter-pg');
 
-// Tạo pool kết nối PostgreSQL
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
-
-// Tạo adapter
-const adapter = new PrismaPg(pool);
-
-// Truyền adapter vào PrismaClient
-const prisma = new PrismaClient({
-  adapter,
-});
+const prisma = new PrismaClient();
 
 module.exports = prisma;
